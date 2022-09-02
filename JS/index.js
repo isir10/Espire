20ed7b8ae5df39de8b025e31e94a791384bd24af
isir10 <isir.malak@moringaschool.com>
2022-09-02 23:45:04 +0300

    added json file

--- JS/index.js
+++ JS/index.js
@@ -2,10 +2,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 	document.querySelector("heading").textContent = "These are cool!!";
 });
-var form = document.getElementById('new');
 
-function myFunction() {
-  if (form.checkValidity()) {
-    alert("Adding Succesful!");
-  }
-};
+let myForm = document.getElementById("new");
+myForm.addEventListener("submit", (e) =>{
+  e.preventDefault()
+
+  let formData =new FormData(e.target)
+  let fullName = document.getElementById("fname").value
+  // alert("djddsa")
+  let myEmail = document.getElementById("email").value
+  // alert(myEmail)
+  
+
+});
+$(document).ready(function(){
+    $("#sub").click(function(){
+      
+          alert("Your request has been received and is being worked on.Further communications on payment and delivery shall be made through your email address entered above ");
+      
+      //  else {
+      //      alert("Please fill in the details for delivery!");
+         
+          
+        
+    })
+  });
