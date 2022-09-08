3918fc4f555525d2749dd756f9686872fa8edf37
isir10 <isir.malak@moringaschool.com>
2022-09-08 17:56:38 +0300

    added a cut remove and cart manupilation function

---
+++ JS/api.js
@@ -0,0 +1,13 @@
+//rapidApi get request//
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6e27dc73b7msha37b4944747157bp1bbd46jsn02e1ca376a29',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,4 @@
-//rapidApi get request//
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6e27dc73b7msha37b4944747157bp1bbd46jsn02e1ca376a29',
-		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	}
-};
-
-fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-    //cart 
+ //cart 
     let cartIcon =document.querySelector("#cart-icon");
     let cart =document.querySelector(".cart");
     let closeCart =document.querySelector("#close-cart");
@@ -24,5 +10,46 @@ fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
     closeCart.onclick= ()=> {
         cart.classList.remove("active");
     };
-    
+
+    //cart working js
+    if (document.readyState== 'loading'){
+        document.addEventListener('DOMContentLoaded', ready)
+    } else {
+        ready();
+    }
+
+    //making  function 
+    function ready(){
+        //remove items from cart
+        var removeCartButtons =document.getElementsByClassName('cart-remove')
+        console.log(removeCartButtons)
+        for (var i =0; i< removeCartButtons.length; i++){
+            var button =removeCartButtons[i]
+            button.addEventListener('click', removeCartItem)
+        }
+    }
+
+    function removeCartItem(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.remove()
+        updateTotal();
+    }
+    //update total
+    function updateTotal(){
+        var cartContent =document.getElementsByClassName("cart-content")[0];
+        var cartBoxes = cartContent.getElementsByClassName("cart-box");
+        var total =0; 
+        for (var i =0; i< cartBoxes.length; i++) {
+            var cartBox= cartBoxes[i];
+            var priceElement= cartBox.getElementsByClassName("cart-price")[0];
+            var quantityElement= cartBox.getElementsByClassName("cart-quantity")[0];
+            var price= parseFloat(priceElement.innerText.replace("ksh", ""));
+            var quantity =quantityElement.value;
+            total= total+ (price*quantity);
+
+
+            document.getElementsByClassName("total-price")[0].innerText= "ksh" + total;
+        }
+
+    }
  
