abf0f3c24e7640b188c4bdce764bea4682ce9680
isir10 <isir.malak@moringaschool.com>
2022-09-08 20:07:44 +0300

    completed adding functinality and alert to cart

--- JS/index.js
+++ JS/index.js
@@ -27,13 +27,86 @@
             var button =removeCartButtons[i]
             button.addEventListener('click', removeCartItem)
         }
+     //quantity  changes
+      var quantityInputs = document.getElementsByClassName('cart-quantity');
+      for (var i =0; i< quantityInputs.length; i++){
+          var input = quantityInputs[i];
+          input.addEventListener('change', quantityChanged);
+      }
+
+      //add to cart
+      var addCart =document.getElementsByClassName("add-cart");
+      for (var i =0; i< addCart.length; i++){
+          var button =addCart[i];
+          button.addEventListener('click', addCartClicked);
+      }
+      // buy buttom work
+      document.getElementsByClassName("btn-buy")[0].addEventListener('click', buyButtonClicked);
+    }
+     
+    function buyButtonClicked(){
+        alert('Your Order is Placed');
+        var cartBoxContent=document.getElementsByClassName("cart-content")[0]
+        while(cartContent.hasChildNodes()){
+        cartContent.removeChild(cartContent.firstChild)
+        }
+        updateTotal();
     }
 
     function removeCartItem(event){
-        var buttonClicked = event.target
-        buttonClicked.parentElement.remove()
+        var buttonClicked = event.target;
+        buttonClicked.parentElement.remove();
         updateTotal();
     }
+    //quantity changes
+    function quantityChanged(event){
+        var input =event.target;
+        if(isNaN(input.value)|| input.value <= 0){
+            input.value = 1
+        }
+        updateTotal()
+    }
+  // add to cart
+  function addCartClicked(event){
+      var button=event.target
+      var shopProducts = button.parentElement
+      var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+      var price = shopProducts.getElementsByClassName("price")[0].innerText;
+      var productImg = shopProducts.getElementsByClassName("product-image")[0].src;
+      addProductToCart(price, title, productImg);
+      updateTotal();
+  }
+
+  function addProductToCart(price, title, productImg){
+      var cartShopBox = document.createElement("div");
+      cartShopBox.classList.add("cart-box");
+      var cartItems = document.getElementsByClassName("cart-content")[0];
+      var cartItemNames =cartItems.getElementsByClassName("cart-product-title");
+      for (var i =0; i< cartItemNames.length; i++){
+          if (cartItemNames[i].innerText== title){
+          alert('you have already added this item to your cart');
+          return;
+      }
+    
+    }
+  
+  var cartBoxContent = `
+                                  <img src="${productImg}" alt="" class="cart-img">
+                            <div class="detail-box">
+                              <div class="cart-product-title">${title}</div>
+                              <div class="cart-price">${price}</div>
+                              <input type="number" value="1" class="cart-quantity">
+                            </div>
+                           <!--Remove Cart-->
+                           <i class='bx bxs-trash-alt cart-remove' ></i>`;
+
+ cartShopBox.innerHTML = cartBoxContent
+ cartItems.append(cartShopBox)
+ cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
+ cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
+
+  }
+
     //update total
     function updateTotal(){
         var cartContent =document.getElementsByClassName("cart-content")[0];
@@ -44,12 +117,13 @@
             var priceElement= cartBox.getElementsByClassName("cart-price")[0];
             var quantityElement= cartBox.getElementsByClassName("cart-quantity")[0];
             var price= parseFloat(priceElement.innerText.replace("ksh", ""));
-            var quantity =quantityElement.value;
-            total= total+ (price*quantity);
-
-
-            document.getElementsByClassName("total-price")[0].innerText= "ksh" + total;
+            var quantity = quantityElement.value;
+            total= total+ price * quantity;
         }
+        
+          document.getElementsByClassName("total-price")[0].innerText = "ksh" + total;
+        
 
     }
+
  
