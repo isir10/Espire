335bd677f210a8b671abfaac8a3ee7b0e07b9a25
isir10 <isir.malak@moringaschool.com>
2022-09-08 16:56:08 +0300

    added on click function to the cart icon

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,28 @@
+//rapidApi get request//
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6e27dc73b7msha37b4944747157bp1bbd46jsn02e1ca376a29',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
+};
 
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
+    //cart 
+    let cartIcon =document.querySelector("#cart-icon");
+    let cart =document.querySelector(".cart");
+    let closeCart =document.querySelector("#close-cart");
+    //open cart
+    cartIcon.onclick= ()=> {
+        cart.classList.add("active");
+    };
+    // close cart
+    closeCart.onclick= ()=> {
+        cart.classList.remove("active");
+    };
+    
  
